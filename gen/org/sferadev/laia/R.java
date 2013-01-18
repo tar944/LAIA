de50ee541d0d85c7304bda09a8fdc4349b9baf4c
SferaDev <sferarc@gmail.com>
2013-01-18 15:56:38 +0100

    Added Files

--- gen/org/sferadev/laia/R.java
+++ gen/org/sferadev/laia/R.java
@@ -15,7 +15,15 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
         public static final int ic_menu_close_clear_cancel=0x7f020002;
         public static final int paypal=0x7f020003;
-        public static final int xda=0x7f020004;
+        public static final int what1=0x7f020004;
+        public static final int what2=0x7f020005;
+        public static final int what3=0x7f020006;
+        public static final int what4=0x7f020007;
+        public static final int what5=0x7f020008;
+        public static final int what6=0x7f020009;
+        public static final int what7=0x7f02000a;
+        public static final int what8=0x7f02000b;
+        public static final int xda=0x7f02000c;
     }
     public static final class id {
         public static final int about=0x7f070012;
