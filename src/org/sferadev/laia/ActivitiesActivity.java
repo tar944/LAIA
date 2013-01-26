365d8ef21eeed9e1078387169706a331fe6aec24
SferaDev <sferarc@gmail.com>
2013-01-26 19:12:03 +0100

    ImageView replaced with WebView in C.3

--- src/org/sferadev/laia/ActivitiesActivity.java
+++ src/org/sferadev/laia/ActivitiesActivity.java
@@ -3,13 +3,40 @@ package org.sferadev.laia;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
+
 
 public class ActivitiesActivity extends Activity {
-
+	
+	String Url1 = "file:///android_asset/activities1.png";
+	String Url3 = "file:///android_asset/activities3.png";
+	String Url4 = "file:///android_asset/activities4.png";
+	String Url5 = "file:///android_asset/activities5.png";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activities);
-	}
+	
+
+	WebView wv1 = (WebView) findViewById(R.id.activities1);
+	wv1.getSettings().setBuiltInZoomControls(true);
+	wv1.loadUrl(Url1);
+	
+	WebView wv3 = (WebView) findViewById(R.id.activities3);
+	wv3.getSettings().setBuiltInZoomControls(true);
+	wv3.loadUrl(Url3);
+	
+	WebView wv4 = (WebView) findViewById(R.id.activities4);
+	wv4.getSettings().setBuiltInZoomControls(true);
+	wv4.loadUrl(Url4);
+	
+	WebView wv5 = (WebView) findViewById(R.id.activities5);
+	wv5.getSettings().setBuiltInZoomControls(true);
+	wv5.loadUrl(Url5);
+	
+	
+	
+}
 
 }
